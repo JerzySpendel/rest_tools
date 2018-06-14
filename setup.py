084230ff7fe5ad0095d0e04e3a680d06311b4d5e
Jerzy Spendel <jerzy.spendel@skygate.io>
2018-06-14 20:34:25 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rest_permissions',
+    version=0.5,
+    packages=find_packages(),
+    author='Jerzy Spendel',
+    author_email='jspendel@gmail.com',
+    description="Django Rest Framework permissions made easy",
+    license='GPL',
+    
+)
